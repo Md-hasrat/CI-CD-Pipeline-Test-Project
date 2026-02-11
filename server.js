a78a67e3033d729e072cffbc7f67ea695c43b60d
Mohammad Hasrat <hasrat233222@gmail.com>
2026-02-11 12:19:39 +0530

    Setup completed for express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+// Parse JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World! This is a CI/CD pipeline test.    ");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+module.exports = app;

6e6112e3ef48b50e9d98986d5ee91de8869ea080
Mohammad Hasrat <hasrat233222@gmail.com>
2026-02-20 14:36:18 +0530

    Some unknown changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Parse JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.get("/", (req, res) => {
   res.send("Hello, World! This is a CI/CD pipeline test.    ");
